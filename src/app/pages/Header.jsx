5f407079e651671b95c7a8bad6c03cb4f4464b39
igoor <igorwallacy3@gmail.com>
2024-03-18 10:40:35 -0300

    mudando a altura da infosingle e colocando perfil do meu insta

--- src/app/pages/Header.jsx
+++ src/app/pages/Header.jsx
@@ -1,6 +1,7 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { IoSearchSharp, IoNotificationsOutline } from 'react-icons/io5';
 import { MdOutlineExpandMore } from 'react-icons/md';
 import Search from './search';
@@ -17,7 +18,7 @@ const Header = () => {
             <Search n={n} setN={setN} />
             <header className="flex justify-between items-center bg-black p-3 px-[4%] absolute top-0 left-0 right-0">
                 <nav className="flex justify-center items-center gap-12">
-                    <Image src="/logo.png" height={70} width={90} />
+                    <Link href='https://www.instagram.com/igor_walace/' target='_blank' ><Image src="/logo.png" height={70} width={90} /></Link>
                     <ul className="flex text-white gap-5" id="ul-header">
                         <li className="cursor-pointer text-slate-300 hover:text-white">
                             Início
