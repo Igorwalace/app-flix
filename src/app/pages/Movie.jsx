817ca9f034559a110d64de1e732ad3eed8bbd340
igoor <igorwallacy3@gmail.com>
2024-03-21 11:29:10 -0300

    final

--- src/app/pages/Movie.jsx
+++ src/app/pages/Movie.jsx
@@ -35,7 +35,7 @@ const Movie = ({
                 modalList={modalList}
             />
 
-            <LocalStorage list={list} setList={setList} />
+            <LocalStorage list={list} setList={setList} /> {/* não funciona (não sei!) */}
 
             {modal && (
                 <div className="fixed top-0 left-0 right-0 bottom-0 bg-[rgb(0,0,0)] flex justify-center items-center z-10">
