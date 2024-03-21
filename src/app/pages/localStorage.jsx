812e45de1a825dc331d69d3ad1ab304f19d20bcd
igoor <igorwallacy3@gmail.com>
2024-03-21 19:15:34 -0300

    final

--- src/app/pages/localStorage.jsx
+++ src/app/pages/localStorage.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from 'react';
 
-export default function LocalStorage({list, setList}) { 
-  useEffect(() => { 
-    if (typeof localStorage == 'undefined') { 
-      const newList = JSON.parse(localStorage.getItem('list')); 
-      setList(newList); 
-    } 
-  }, []);
-  1useEffect(() => {
-2    localStorage.setItem('list', JSON.stringify(list));
-3}, [list]);
-4
-5return null;
+export default function LocalStorage({ list, setList }) {
+    useEffect(() => {
+        if (typeof localStorage == 'undefined') {
+            const newList = JSON.parse(localStorage.getItem('list'));
+            setList(newList);
+        }
+    }, []);
+    useEffect(()=>{
+        localStorage.setItem('list', JSON.stringify(list))
+    }, [list])
 }
