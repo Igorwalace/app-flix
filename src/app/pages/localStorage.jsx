3c6d2ae8e7e14ffbbadc2aeed2a21548086093d7
igor_walace <131620215+Igorwalace@users.noreply.github.com>
2024-03-21 12:23:10 -0300

    Atualizar o localStorage.jsx

--- src/app/pages/localStorage.jsx
+++ src/app/pages/localStorage.jsx
@@ -2,21 +2,24 @@ import React, { useEffect, useState } from 'react';
 
 export default function LocalStorage({ list: initialList, setList }) {
   const [list, setLocalList] = useState(initialList || []);
-
-  useEffect(() => {
-    const storedList = localStorage.getItem('list');
-    if (storedList) {
-      setLocalList(JSON.parse(storedList));
-    }
-  }, [initialList]);
-
-  useEffect(() => {
-    localStorage.setItem('list', JSON.stringify(list));
-  }, [list]);
-
-  useEffect(() => {
-    setList(list);
-  }, [list]);
+5
+6  useEffect(() => {
+7    const storedList = localStorage.getItem('list');
+8    if (storedList) {
+9      const newList = JSON.parse(storedList);
+10      if (Array.isArray(newList)) {
+11        setLocalList(newList);
+12      }
+13    }
+14  }, [initialList]);
+15
+16  useEffect(() => {
+17    localStorage.setItem('list', JSON.stringify(list));
+18  }, [list]);
+19
+20  useEffect(() => {
+21    setList(list);
+22  }, [list]);
 
   return null;
 }
